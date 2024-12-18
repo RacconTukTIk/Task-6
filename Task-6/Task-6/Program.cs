0ddc0f005c23f93008bb811a2fc54999b75a9578
Galdin Ivan <galdin.2005@mail.ru>
2024-12-18 17:29:00 +0300

    update

--- Task-6/Task-6/Program.cs
+++ Task-6/Task-6/Program.cs
@@ -15,13 +15,14 @@ namespace Task_6
 
         static void CheckFermat()
         {
+
             for (int n = 1; n < 4; n++)
             {
                 for (int a = 1; a < 10; a++)
                 {
                     for (int b = 1; b < 10; b++)
                     {
-                        for (int c = 1; c < 10; c++)
+                        for (int c = 1; c  10; c++)
                         {
                             if (CheckFermat(a, b, c, n))
                             {
