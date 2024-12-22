dbcd7551c5de9757f4606f0690e90e5f886e1566
Владислав Латышев <v.l.1979@bk.ru>
2024-12-22 16:12:59 +0300

    1

--- Task-6/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ Task-6/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a17bbcb788b9249755d59d7f6c08276b4275649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d623ab9ace53b6ef96899b0ae446f845967cfbeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
